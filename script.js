80eb219e061cec5f11c1af33847e0cf68bcb5fca
makhotkinaangelina <gelyamakh@gmail.com>
2026-09-18 11:35:29 +0300

    fix(theme): persist selected theme

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const themeSwitch = document.querySelector('.theme-switch');
 
-const savedTheme = localStorage.getItem('theme') || 'light';
-
-document.documentElement.dataset.theme = savedTheme;
-
 themeSwitch.addEventListener('click', () => {
     const currentTheme = document.documentElement.dataset.theme;
     const newTheme = currentTheme === 'light' ? 'dark' : 'light';
